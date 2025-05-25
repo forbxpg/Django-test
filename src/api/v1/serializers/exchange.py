74559bbd9c8b3f2fdd29458bd9e8a61c5df19a5f
forbxpg <dwizard80@gmail.com>
2025-05-25 13:34:59 +0300

    Create ExchangeWriteSerializer

--- src/api/v1/serializers/exchange.py
+++ src/api/v1/serializers/exchange.py
@@ -2,18 +2,63 @@
 
 from rest_framework import serializers
 
+from ads.services import get_not_exchanged_ads_queryset
 from exchanges.models import ExchangeProposal
 
 
+class ExchangeWriteSerializer(serializers.ModelSerializer):
+    """Сериализатор для записи предложений обмена."""
 
-class ExchangeSerializer(serializers.ModelSerializer):
-    """Сериализатор для предложений обмена."""
+    ad_sender = serializers.PrimaryKeyRelatedField(
+        queryset=get_not_exchanged_ads_queryset(),
+        write_only=True,
+    )
+    ad_receiver = serializers.PrimaryKeyRelatedField(
+        queryset=get_not_exchanged_ads_queryset(),
+        write_only=True,
+    )
 
     class Meta:
         model = ExchangeProposal
         fields = (
-            "id",
             "ad_sender",
             "ad_receiver",
-
+            "comment",
         )
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=ExchangeProposal.objects.all(),
+                fields=("ad_sender", "ad_receiver"),
+                message="Предложение обмена уже существует.",
+            )
+        ]
+
+    def validate(self, attrs):
+        """Проверка на возможность обмена."""
+        ad_sender = attrs.get("ad_sender")
+        ad_receiver = attrs.get("ad_receiver")
+        if ad_sender == ad_receiver:
+            raise serializers.ValidationError(
+                "Нельзя обменивать объявление на само себя."
+            )
+        if ad_sender.is_exchanged or ad_receiver.is_exchanged:
+            raise serializers.ValidationError(
+                "Нельзя обмениваться уже обмененными объявлениями."
+            )
+        if (
+            ExchangeProposal.objects.filter(
+                ad_sender=ad_sender,
+                ad_receiver=ad_receiver,
+            ).exists()
+            or ExchangeProposal.objects.filter(
+                ad_sender=ad_receiver,
+                ad_receiver=ad_sender,
+            ).exists()
+        ):
+            raise serializers.ValidationError(
+                "Предложение обмена уже существует для этих объявлений."
+            )
+        return attrs
+
+
+
