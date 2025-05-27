450a5feaaca45f8c934340d61cb4fabda0f0943b
forbxpg <dwizard80@gmail.com>
2025-05-27 13:08:04 +0300

    Run Postgres in Docker

--- src/share_platform/settings.py
+++ src/share_platform/settings.py
@@ -99,12 +99,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "share_platform.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if os.environ.get("USE_SQLITE", default="true").lower() == "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+
+if os.environ.get("USE_POSTGRES", default="false").lower() == "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB", default="share_platform"),
+            "USER": os.environ.get("POSTGRES_USER", default="postgres"),
+            "PASSWORD": os.environ.get(
+                "POSTGRES_PASSWORD", default="password"
+            ),
+            "HOST": os.environ.get("POSTGRES_HOST", default="localhost"),
+            "PORT": os.environ.get("POSTGRES_PORT", default="5432"),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
